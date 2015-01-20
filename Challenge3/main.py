9806c1b99154f800d1eca10c368d6d57b87085fa
ddsnowboard <ddsnowboard@gmail.com>
2015-01-20 15:40:18 -0600

    It's all screwed up.

--- Challenge3/main.py
+++ Challenge3/main.py
@@ -1,1 +1,11 @@
-# Make a sieve, and then go through it and find all the palindromes, and then print the highest out out. 
+# Make a sieve, and then go through it and find all the palindromes, and then print the highest out out.
+def sieve(n):
+    l = (n+1) * [True]
+    for i, j in enumerate(l):
+        if j:
+            p = 2
+            while i*p <= n:
+                print(p)
+                l[j] = False
+                p += 1
+    return l
