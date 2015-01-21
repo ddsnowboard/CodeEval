707998a185e6dd34427094bef674dbfc6f6a857f
ddsnowboard <ddsnowboard@gmail.com>
2015-01-20 22:05:21 -0600

    Finished challenge3

--- Challenge3/main.py
+++ Challenge3/main.py
@@ -1,11 +1,17 @@
 # Make a sieve, and then go through it and find all the palindromes, and then print the highest out out.
 def sieve(n):
-    l = (n+1) * [True]
-    for i, j in enumerate(l):
-        if j:
-            p = 2
-            while i*p <= n:
-                print(p)
-                l[j] = False
-                p += 1
-    return l
+	l = (n+1) * [True]
+	for i, j in enumerate(l):
+		if i == 0:
+			l[i] = False
+			continue
+		elif i == 1:
+			l[i] = None
+			continue
+		if j:
+			p = 2
+			while i*p <= n:
+				l[i*p] = False
+				p += 1
+	return l
+print(max([i[0] for i in enumerate(sieve(1000)) if (str(i[0]) == str(i[0])[::-1]) and (i[1])]))
