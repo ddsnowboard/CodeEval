03547c82e4d1b590818ff9a126b02a94cae46d8e
ddsnowboard <ddsnowboard@gmail.com>
2015-01-22 20:11:36 -0600

    Finished 83.

--- Challenge83/main.py
+++ Challenge83/main.py
@@ -1,8 +1,15 @@
 from sys import argv
 import re
+from collections import Counter
+from functools import reduce
+not_letters = re.compile(r"[^a-z]")
 with open(argv[1], 'r') as f:
-	not_letters = re.compile(r"[^a-z]")
 	for l in f:
-		current_line = not_letters.sub("",l.lower()).strip("\n")
-		print(current_line)
-	input()
+		current_line = not_letters.sub("",l.lower())
+		c = Counter(current_line)
+		scores = {}
+		next_score = 26
+		for i in [j[0] for j in c.most_common()]:
+			scores[i] = next_score
+			next_score -= 1
+		print(reduce(lambda x, y: x + scores[y], current_line, 0))
