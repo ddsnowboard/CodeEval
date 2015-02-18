253dcc62090c4209c15969f4461efd91c2904244
ddsnowboard <ddsnowboard@gmail.com>
2015-02-17 21:01:43 -0600

    Set up profiling because it is taking forever.
    I don't know what is making it take so long. I turned the list comp into
    a generator to save memory, but it still takes forever.

--- Challenge144/main.py
+++ Challenge144/main.py
@@ -1,11 +1,27 @@
 from sys import argv
 from collections import defaultdict
+def powers(a, n):
+	x = 1
+	current_output = a
+	while x<=n:
+		yield current_output
+		current_output *= a
+		x+=1
+totals = defaultdict(int)
+import cProfile as pro
+pr = pro.Profile()
 with open(argv[1], 'r') as f:
-	for i in f:
-		a, n = (int(p) for p in i.split(" "))
-		totals = defaultdict(int)
-		for i in [a**x for x in range(n+1)]:
-			totals[str(i)[-1]]+=1
-# Make it print all the numbers, even the ones that don't show up. 
-# https://www.codeeval.com/open_challenges/144/
-print(':'.join(['{}, {}'.format(i,j) for i, j in totals.items()]))
+	for l in f:
+		pr.enable()
+		a, n = (int(p) for p in l.split(" "))
+		# counter = 0
+		for i in powers(a, n):
+			totals[i%10]+=1
+			# counter += 1
+			# if counter == 1000:
+				# print("{}\n\n".format(i))
+				# counter = 0
+		pr.disable()
+		pr.print_stats()
+		print(', '.join(['{}:{}'.format(i,totals[i]) for i in range(10)]))
+input()
